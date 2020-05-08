3da83f3622bac35548b8821cd4196c106d247c7d
Christoph Steiner <gawen@t-online.de>
2020-05-08 16:53:15 +0200

    update

--- src/App.js
+++ src/App.js
@@ -7,9 +7,12 @@ import './App.css';
 
 function App() {
 const [data, setData] = useState([
-   {restaurant: {id: "1", name: "Restaurant 1", city_id: "city Id 1", comment_id: "comment_id 1"}, comment: {id: "1", text: "text", date: "date"}, city: {id: "1", name:"name"}, restaurant_has_tag: {id_restaurant: "1", id_tag:"1"}, tag: { id: "1", name: "name 1"}},
-   {restaurant: {id: "2", name: "Restaurant 2", city_id: "city Id 2", comment_id: "comment_id 2"}, comment: {id: "2", text: "text", date: "date"}, city: {id: "2", name:"name"}, restaurant_has_tag: {id_restaurant: "2", id_tag:"2"}, tag: { id: "2", name: "name 2"}},
-   {restaurant: {id: "3", name: "Restaurant 3", city_id: "city Id 3", comment_id: "comment_id 3"}, comment: {id: "3", text: "text", date: "date"}, city: {id: "3", name:"name"}, restaurant_has_tag: {id_restaurant: "3", id_tag:"3"}, tag: { id: "3", name: "name 3"}}
+   {restaurant: {id: "1", name: "Restaurant 1", city_id: "city Id 1", comment_id: "comment_id 1"}, comment: {id: "1", text: "This is a random comment for a restaurant", date: "date"}, city: {id: "1", name:"name"}, restaurant_has_tag: {id_restaurant: "1", id_tag:"1"}, tag: { tag_id: "1", name: "name 1"}},
+   {restaurant: {id: "2", name: "Restaurant 2", city_id: "city Id 2", comment_id: "comment_id 2"}, comment: {id: "2", text: "This is a random comment for a restaurant", date: "date"}, city: {id: "2", name:"name"}, restaurant_has_tag: {id_restaurant: "2", id_tag:"2"}, tag: { tag_id: "2", name: "name 2"}},
+   {restaurant: {id: "3", name: "Restaurant 3", city_id: "city Id 3", comment_id: "comment_id 3"}, comment: {id: "3", text: "This is a random comment for a restaurant", date: "date"}, city: {id: "3", name:"name"}, restaurant_has_tag: {id_restaurant: "3", id_tag:"3"}, tag: { tag_id: "3", name: "name 3"}},
+   {restaurant: {id: "4", name: "Restaurant 4", city_id: "city Id 4", comment_id: "comment_id 4"}, comment: {id: "4", text: "This is a random comment for a restaurant", date: "date"}, city: {id: "4", name:"name"}, restaurant_has_tag: {id_restaurant: "4", id_tag:"4"}, tag: { tag_id: "4", name: "name 1"}},
+   {restaurant: {id: "2", name: "Restaurant 2", city_id: "city Id 2", comment_id: "comment_id 2"}, comment: {id: "5", text: "This is a random comment for a restaurant", date: "date"}, city: {id: "5", name:"name"}, restaurant_has_tag: {id_restaurant: "5", id_tag:"5"}, tag: { tag_id: "5", name: "name 5"}},
+   {restaurant: {id: "6", name: "Restaurant 6", city_id: "city Id 6", comment_id: "comment_id 6"}, comment: {id: "6", text: "This is a random comment for a restaurant", date: "date"}, city: {id: "6", name:"name"}, restaurant_has_tag: {id_restaurant: "6", id_tag:"6"}, tag: { tag_id: "6", name: "name 6"}}
 ])
 
 let history = useHistory() 
@@ -26,17 +29,15 @@ const handleClick =(id) => {
   history.push(`/details/${id}`);
 }
 
-
-
-
-
-
-
+const handleHome =() => {
+  history.push(`/`);
+  window.location.reload()  
+}
 
   return (
     <>
       <div className="wrapper">
-      <h1>YELP!mini</h1>
+      <h1 onClick={handleHome}>YELP!mini</h1>
       <h4>by group 3</h4>
       <Search onHandleSubmit={handleSubmit}/>
       <Switch>
--- src/components/details.js
+++ src/components/details.js
@@ -1,11 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Map, TileLayer } from "react-leaflet";
+import restaurant_image from '../img/restaurant.jpg'
 import '../App.css';
 
 
+
 function Details (props) {
-  
-console.log(props.match.params.id)
+const [lat, setLat] = useState(0)
+const [lon, setLon] = useState(0)
+
     return (
+    <>
      <div className="details">
         {props.data
             .filter(restaurant =>
@@ -14,15 +19,24 @@ console.log(props.match.params.id)
                 : restaurant
             )
             .map(restaurant => (
-                <>
-                <div className="restaurant" key={restaurant.restaurant.id+restaurant.restaurant.name}>
-                <h2>{restaurant.restaurant.name}</h2>
-                <p>{restaurant.tag.id_tag}</p>
-                <p>{restaurant.comment.text}</p>
-            </div>
+            <>
+                <div className="details2" key={restaurant.restaurant.id+restaurant.restaurant.name}>
+                    <img src={restaurant_image} alt="logo"></img>
+                    <h2>{restaurant.restaurant.name}</h2>
+                    <p>Tags: {restaurant.tag.name}</p>
+                    <p>Comments: {restaurant.comment.text}</p>,
+                </div>
             </>
             ))}
      </div>
+     {/* <div className="map">
+            <Map center={[lat, lon]} zoom={13} zoomControl={false}>
+                <TileLayer
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                    attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'/>
+        </Map>
+    </div> */}
+    </>
     );
   }
   
